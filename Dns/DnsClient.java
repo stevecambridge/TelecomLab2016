92375a4ad592038ebdac027aee80e73a7d5ff446
stevecambridge <stevecambridge27@hotmail.com>
2016-10-02 14:07:25 -0400

    began working on send/recv loop

--- Dns/DnsClient.java
+++ Dns/DnsClient.java
@@ -1,21 +1,45 @@
 package Dns;
 
 import java.net.InetAddress;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
 
 public class DnsClient {
 
 	public static void main(String[] args) throws Exception {
 
 		DnsArgs params = new DnsArgs(args);
-		System.out.println(params.timeout);
-		System.out.println(params.retries);
-		System.out.println(params.port);
-		System.out.println(params.mailServer);
-		System.out.println(params.nameServer);
-		System.out.println(params.ip);
-		System.out.println(params.name);
 
-		InetAddress testaddr = InetAddress.getByAddress(params.ip);
+		//Print sending information
+		System.out.println("DnsClient sending request for " + params.name);
+		System.out.println("Server: " + params.ip[0] + "." + params.ip[1] + "." + params.ip[2] + "." + params.ip[3]);
+		System.out.println("Request type: " + (params.nameServer ? "NS" : "") + (params.mailServer ? "MX" : "") + (params.mailServer || params.nameServer ? "" : "A"));
+
+		//get the address of the DNS server
+		InetAddress targetAddr = InetAddress.getByAddress(params.ip);
+
+		//initialize send buffer
+		byte[] sendData = new byte[1024];
+		byte[] recvData = new byte[1024];
+
+		//initialize socket and datagram packet
+		DatagramSocket dSock = new DatagramSocket();
+		DatagramPacket sendPkt = new DatagramPacket(sendData, sendData.length, targetAddr, params.port);
+		DatagramPacket recvPkt = new DatagramPacket(recvData, recvData.length);
+
+		int tries = 0;
+		while(tries <= params.retries) {
+			dSock.send(sendPkt);
+			dSock.setSoTimeout((int)(params.timeout*1000));
+
+			dsock.receive()
+
+
+		}
+
+
+
+		
 
 		return;
 
